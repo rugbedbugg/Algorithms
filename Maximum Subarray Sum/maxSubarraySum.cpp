bad6354d3d3f66533ff0b9a876d3b959a5e5d716
Oxide 1-6 <yes.par781@gmail.com>
2026-01-08 04:37:07 +0530

    Added Maximum Subarray Sum Problem analysis

---
+++ Maximum Subarray Sum/maxSubarraySum.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+///////////////////////
+/// Cross sum logic ///
+///////////////////////
+int getMaxCrossSum(const vector<int>& arr, int low, int mid, int high) {
+	// --- Initialize sum --------- // 
+	long long sum = 0;
+
+	// --- Left side -------------- //
+	long long leftSide = INT_MIN;
+	sum = 0;
+	for (int i=mid; i>=low; i--) {
+		sum += arr[i];
+		leftSide = max(leftSide, sum);
+	}
+	// --- Right side ------------- //
+	long long rightSide = INT_MIN;
+	sum = 0;
+	for (int i=mid+1; i<=high; i++) {
+		sum += arr[i];
+		rightSide = max(rightSide,sum);
+	}
+	// --- Debugging --- //
+	// cout << leftSide << endl;
+	// cout << rightSide << endl;
+	// cout << leftSide+rightSide << endl << endl;
+	
+	return (int)(leftSide + rightSide);
+}
+///////////////////
+/// Propagation ///
+///////////////////
+int getMaxSubarraySum(const vector<int>& arr, int low, int high) {
+	if (low == high) return arr[low];
+
+	// --- Initialize mid -------- //
+	int mid = low + (high-low)/2;
+
+	// --- Get the maximum possible subarray ------- //
+	// --- CASE 1: Left subarray sum is largest ---- //
+	// --- CASE 2: Right subarray sum is largest --- //
+	// --- CASE 3: Cross subarray sum is largest --- //
+	return max({
+		getMaxSubarraySum(arr, low, mid),
+		getMaxSubarraySum(arr, mid+1, high),
+		getMaxCrossSum(arr, low, mid, high)
+	});
+}
+/////////////////
+/// Intiation ///
+/////////////////
+int maxSubarraySum(vector<int>& arr) {
+	return getMaxSubarraySum(arr, 0, arr.size()-1);
+}
+
+///////////////////
+/// Driver Code ///
+///////////////////
+int main() {
+	vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+	int maxSum = maxSubarraySum(arr);
+
+	cout << "Maximum sub-array sum possible in given array is: ";
+	cout << maxSum << endl;
+
+	return 0;
+}
